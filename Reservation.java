3e2da71b5c339e7e1b80cf98745a109c74e03c4d
davidleuciuc <48672940+davidleuciuc@users.noreply.github.com>
2019-04-08 09:30:02 +0300

    Add files via upload

---
+++ Reservation.java
@@ -0,0 +1,52 @@
+package org.loose.fis.reservation.manager.model;
+
+import java.util.Objects;
+
+/**
+* This is the reservation model.
+* It is the core entity of our software system.
+*/
+
+public class Reservation {
+    private String id;
+    private User user;
+    private String message;
+
+    public Reservation(User user, String message) {
+        this.user = user;
+        this.message = message;
+    }
+
+    @Override public String toString() {
+        return "Reservation{" + "user=" + user + ", message='" + message + '\'' + '}';
+    }
+
+    @Override public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Reservation that = (Reservation) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ User.java
@@ -0,0 +1,81 @@
+package org.loose.fis.reservation.manager.model;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class User {
+    private String id;
+    private String firstName;
+    private String lastName;
+    // optional field (only if users will require it)
+    private String nickName;
+    private int age;
+    
+    // field that says if the user owns a company that you can get reservations to
+   private boolean isOwner;
+
+    
+    public User(String firstName, String lastName, int age) {
+        id = UUID.randomUUID().toString();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override public String toString() {
+        return "User{" + "firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + '}';
+    }
+
+    @Override public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isOwner() {
+        return isOwner;
+    }
+
+    public void setOwner(boolean owner) {
+        isOwner = owner;
+    }
+}
